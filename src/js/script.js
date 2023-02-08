c629796dc4bd9bac8ce78f404cef164a4fc94176
Adi <Adiletsharshekeev@yahoo.de>
2023-02-08 17:31:40 +0100

    next block

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
   $('.corousel__inner').slick({
     speed: 1200,
-    adaptiveHeight: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
     responsive: [
